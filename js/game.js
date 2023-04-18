a3aa27cda7b7003d964ec97bd62ca4bd1b1a15e8
bartwisch <123270793+bartwisch@users.noreply.github.com>
2023-04-18 19:25:37 +0000

    gegner schießen

--- js/game.js
+++ js/game.js
@@ -8,53 +8,50 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-    let audioContextActivated = false;
-    document.addEventListener('keydown', function () {
-        if (!audioContextActivated) {
-            audioContext.resume();
-            audioContextActivated = true;
-        }
-    });
-    document.addEventListener('click', function () {
-        audioContext.resume();
-    });
-
-
     function playShootSound() {
         const oscillator = audioContext.createOscillator();
         const gainNode = audioContext.createGain();
-    
+
         oscillator.type = 'square';
         oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
         gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
-    
+
         oscillator.connect(gainNode);
         gainNode.connect(audioContext.destination);
-    
+
         oscillator.start();
         setTimeout(() => {
             oscillator.stop();
         }, 100);
     }
-    
+
     function playHitSound() {
         const oscillator = audioContext.createOscillator();
         const gainNode = audioContext.createGain();
-    
+
         oscillator.type = 'sine';
         oscillator.frequency.setValueAtTime(880, audioContext.currentTime);
         gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
-    
+
         oscillator.connect(gainNode);
         gainNode.connect(audioContext.destination);
-    
+
         oscillator.start();
         setTimeout(() => {
             oscillator.stop();
         }, 100);
     }
-    
-    
+
+    const enemyBullets = [];
+
+    function createEnemyBullet(position, material) {
+        const bullet = BABYLON.MeshBuilder.CreateBox('enemyBullet', { width: 10, height: 2, depth: 1 }, scene);
+        bullet.position.copyFrom(position);
+        bullet.position.z = 1;
+        bullet.material = material;
+        enemyBullets.push(bullet);
+        playShootSound();
+    }
 
     const createScene = function () {
         const scene = new BABYLON.Scene(engine);
@@ -98,7 +95,6 @@ window.addEventListener('DOMContentLoaded', function () {
                 playShootSound();
             }
         };
-        
 
         scene.onKeyboardObservable.add(({ event }) => onKeyDown(event), BABYLON.KeyboardEventTypes.KEYDOWN);
 
@@ -118,58 +114,72 @@ window.addEventListener('DOMContentLoaded', function () {
                 }
             });
 
+            // Update enemy bullets
+            enemyBullets.forEach((bullet, index) => {
+                bullet.position.x -= 5;
+                if (bullet.position.x < camera.position.x - canvas.width / 2) {
+                    bullet.dispose();
+                    enemyBullets.splice(index, 1);
+                }
+            });
+
             // Update enemies
             enemies.forEach((enemy, index) => {
                 enemy.position.x -= 2;
                 if (enemy.position.x < camera.position.x - canvas.width / 2) {
                     enemy.dispose();
-                    enemies.splice(index, 
-                        1);
-                    }
-                    });
-                            // Spawn enemies
-        enemySpawnCounter++;
-        if (enemySpawnCounter >= 100) {
-            const enemyMaterial = new BABYLON.StandardMaterial('enemyMaterial', scene);
-            enemyMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
-
-            const enemy = BABYLON.MeshBuilder.CreateBox('enemy', { width: 30, height: 30, depth: 1 }, scene);
-            enemy.position.x = camera.position.x + canvas.width / 2;
-            enemy.position.y = Math.random() * (canvas.height - 60) - (canvas.height / 2 - 30);
-            enemy.position.z = 1;
-            enemy.material = enemyMaterial;
-            enemies.push(enemy);
-
-            enemySpawnCounter = 0;
-        }
-
-        // Detect collisions between bullets and enemies
-        bullets.forEach((bullet, bulletIndex) => {
-            enemies.forEach((enemy, enemyIndex) => {
-                if (Math.abs(bullet.position.x - enemy.position.x) < 20 &&
-                    Math.abs(bullet.position.y - enemy.position.y) < 20) {
-                    bullet.dispose();
-                    bullets.splice(bulletIndex, 1);
-        
-                    enemy.dispose();
-                    enemies.splice(enemyIndex, 1);
-        
-                    playHitSound();
+                    enemies.splice(index, 1);
                 }
             });
+
+            // Spawn enemies
+            enemySpawnCounter++;
+            if (enemySpawnCounter >= 100) {
+                const enemyMaterial = new BABYLON.StandardMaterial('enemyMaterial', scene);
+                enemyMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
+
+                const enemy = BABYLON.MeshBuilder.CreateBox('enemy', { width: 30, height: 30, depth: 1 }, scene);
+                enemy.position.x = camera.position.x + canvas.width / 2;
+                enemy.position.y = Math.random() * (canvas.height - 60) - (canvas.height / 2 - 30);
+                enemy.position.z = 1;
+                enemy.material = enemyMaterial;
+                enemies.push(enemy);
+
+                enemySpawnCounter = 0;
+
+                // Make enemy shoot
+                setTimeout(() => {
+                    createEnemyBullet(enemy.position, spaceshipMaterial);
+                }, 1000);
+            }
+
+            // Detect collisions between bullets and enemies
+            bullets.forEach((bullet, bulletIndex) => {
+                enemies.forEach((enemy, enemyIndex) => {
+                    if (Math.abs(bullet.position.x - enemy.position.x) < 20 &&
+                        Math.abs(bullet.position.y - enemy.position.y) < 20) {
+                        bullet.dispose();
+                        bullets.splice(bulletIndex, 1);
+
+                        enemy.dispose();
+                        enemies.splice(enemyIndex, 1);
+
+                        playHitSound();
+                    }
+                });
+            });
         });
+
+        return scene;
+    };
+
+    const scene = createScene();
+
+    engine.runRenderLoop(function () {
+        scene.render();
     });
 
-    return scene;
-};
-
-const scene = createScene();
-
-engine.runRenderLoop(function () {
-    scene.render();
-});
-
-window.addEventListener('resize', function () {
-    engine.resize();
-});
+    window.addEventListener('resize', function () {
+        engine.resize();
+    });
 });
