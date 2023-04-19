cbe92534cad60d21b9fcdc0e101e69835957af35
bartwisch <123270793+bartwisch@users.noreply.github.com>
2023-04-19 21:45:35 +0200

    c1

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('DOMContentLoaded', function () {
     const canvas = document.createElement('canvas');
     canvas.width = 800;
@@ -6,7 +7,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const engine = new BABYLON.Engine(canvas, true); 
 
-  const enemyBullets = [];
+    const enemyBullets = [];
 
     function createEnemyBullet(position, material) {
         const bullet = BABYLON.MeshBuilder.CreateBox('enemyBullet', { width: 10, height: 2, depth: 1 }, scene);
@@ -16,14 +17,43 @@ window.addEventListener('DOMContentLoaded', function () {
         enemyBullets.push(bullet);
         playShootSound();
     }
-  
 
-    
+    function createExplosion(position, scene) {
+        const particleSystem = new BABYLON.ParticleSystem("particles", 2000, scene);
+
+        // Texturen laden
+        particleSystem.particleTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/flare.png", scene);
+
+        // E//instellungen
+        particleSystem.minAngularSpeed = 0;
+        particleSystem.maxAngularSpeed = 2 * Math.PI;
+        particleSystem.minSize = 0.1;
+        particleSystem.maxSize = 0.5;
+        particleSystem.minLifeTime = 0.3;
+        particleSystem.maxLifeTime = 1.5;
+        particleSystem.minEmitPower = 1;
+        particleSystem.maxEmitPower = 3;
+        particleSystem.emitter = position;
+        particleSystem.emitRate = 200;
+        particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+        particleSystem.gravity = new BABYLON.Vector3(0, -9.81, 0);
+        particleSystem.direction1 = new BABYLON.Vector3(-1, 1, 0);
+        particleSystem.direction2 = new BABYLON.Vector3(1, -1, 0);
+
+        // Starten der Partikel-Animation
+        particleSystem.start();
+
+        // Partikel-Animation stoppen nach 2 Sekunden
+        setTimeout(() => {
+            particleSystem.stop();
+        }, 2000);
+    }
 
     const createScene = function () {
         const scene = new BABYLON.Scene(engine);
+
         scene.clearColor = new BABYLON.Color3(0, 0, 1);
-    
+
         const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, -100), scene);
         camera.setTarget(BABYLON.Vector3.Zero());
         camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
@@ -31,18 +61,18 @@ window.addEventListener('DOMContentLoaded', function () {
         camera.orthoBottom = -canvas.height / 2;
         camera.orthoLeft = -canvas.width / 2;
         camera.orthoRight = canvas.width / 2;
-    
+
         const spaceshipMaterial = new BABYLON.StandardMaterial('spaceshipMaterial', scene);
         spaceshipMaterial.diffuseColor = new BABYLON.Color3(1, 1, 1);
-    
+
         const spaceship = BABYLON.MeshBuilder.CreateBox('spaceship', { width: 50, height: 30, depth: 1 }, scene);
         spaceship.position.x = -350;
         spaceship.position.z = 1;
         spaceship.material = spaceshipMaterial;
-    
+
         const bullets = [];
         const enemies = [];
-    
+
         const onKeyDown = function (event) {
             if (event.key === 'ArrowUp') {
                 spaceship.position.y += 10;
@@ -62,37 +92,37 @@ window.addEventListener('DOMContentLoaded', function () {
                 playShootSound();
             }
         };
-    
+
         scene.onKeyboardObservable.add(({ event }) => onKeyDown(event), BABYLON.KeyboardEventTypes.KEYDOWN);
-    
+
         // Touch-Event-Listener hinzufügen
         canvas.addEventListener('touchstart', (event) => {
             event.preventDefault();
             const touch = event.touches[0];
             const touchX = touch.clientX - canvas.width / 2;
             const touchY = -touch.clientY + canvas.height / 2;
-    
+
             spaceship.position.x = touchX;
             spaceship.position.y = touchY;
         });
-    
+
         canvas.addEventListener('touchmove', (event) => {
             event.preventDefault();
             const touch = event.touches[0];
             const touchX = touch.clientX - canvas.width / 2;
             const touchY = -touch.clientY + canvas.height / 2;
-    
+
             spaceship.position.x = touchX;
             spaceship.position.y = touchY;
         });
-    
+
         let enemySpawnCounter = 0;
-    
+
         scene.registerBeforeRender(() => {
             // Sidescrolling
             camera.position.x += 1;
             spaceship.position.x += 1;
-    
+
             // Update bullets
             bullets.forEach((bullet, index) => {
                 bullet.position.x += 5;
@@ -101,7 +131,7 @@ window.addEventListener('DOMContentLoaded', function () {
                     bullets.splice(index, 1);
                 }
             });
-    
+
             // Update enemy bullets
             enemyBullets.forEach((bullet, index) => {
                 bullet.position.x -= 5;
@@ -110,7 +140,7 @@ window.addEventListener('DOMContentLoaded', function () {
                     enemyBullets.splice(index, 1);
                 }
             });
-    
+
             // Update enemies
             enemies.forEach((enemy, index) => {
                 enemy.position.x -= 2;
@@ -119,31 +149,28 @@ window.addEventListener('DOMContentLoaded', function () {
                     enemies.splice(index, 1);
                 }
             });
-    
+
             // Spawn enemies
             enemySpawnCounter++;
             if (enemySpawnCounter >= 100) {
                 const enemyMaterial = new BABYLON.StandardMaterial('enemyMaterial', scene);
                 enemyMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // set the diffuse color to red
-                
+
                 const enemy = BABYLON.MeshBuilder.CreateBox('enemy', { width: 30, height: 30, depth: 1 }, scene);
                 enemy.position.x = camera.position.x + canvas.width / 2;
                 enemy.position.y = Math.random() * (canvas.height - 60) - (canvas.height / 2 - 30);
                 enemy.position.z = 1;
                 enemy.material = enemyMaterial; // assign the material to the enemy
                 enemies.push(enemy);
-                
-              
-                
-    
+
                 enemySpawnCounter = 0;
-    
+
                 // Make enemy shoot
                 setTimeout(() => {
                     createEnemyBullet(enemy.position, spaceshipMaterial);
                 }, 1000);
             }
-    
+
             // Detect collisions between bullets and enemies
             bullets.forEach((bullet, bulletIndex) => {
                 enemies.forEach((enemy, enemyIndex) => {
@@ -151,21 +178,22 @@ window.addEventListener('DOMContentLoaded', function () {
                         Math.abs(bullet.position.y - enemy.position.y) < 20) {
                         bullet.dispose();
                         bullets.splice(bulletIndex, 1);
-    
+
                         enemy.dispose();
                         enemies.splice(enemyIndex, 1);
-    
+
+                        createExplosion(enemy.position, scene); // Explosion erzeugen
+
                         playHitSound();
                     }
                 });
             });
         });
-    
+
         return scene;
     };
-    
+
     const scene = createScene();
-    
 
     engine.runRenderLoop(function () {
         scene.render();
