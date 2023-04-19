77e1ae0da3fda41aec0c60e84ed9253dba93d529
bartwisch <123270793+bartwisch@users.noreply.github.com>
2023-04-19 18:39:06 +0000

    sound code getrennt

---
+++ js/sound.js
@@ -0,0 +1,36 @@
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+
+function playShootSound() {
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+    }, 100);
+}
+
+function playHitSound() {
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(880, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+    }, 100);
+}
